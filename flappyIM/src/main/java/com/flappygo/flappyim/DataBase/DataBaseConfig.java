0dabb901a3ac1659feeddd709dc21a41fd768efd
lilin <327603258@qq.com>
2024-04-08 15:35:44 +0800

    change database version

--- flappyIM/src/main/java/com/flappygo/flappyim/DataBase/DataBaseConfig.java
+++ flappyIM/src/main/java/com/flappygo/flappyim/DataBase/DataBaseConfig.java
@@ -8,7 +8,7 @@ public class DataBaseConfig {
     //数据库名称
     public static String DB_NAME = "com.flappygo.flappyim.db";
     //数据库版本
-    public static int DB_VERSION = 1;
+    public static int DB_VERSION = 2;
     //消息表
     public static String TABLE_MESSAGE = "message";
     //会话表
